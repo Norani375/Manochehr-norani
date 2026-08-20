af9cb7833e4610fbd1d2480e9e885e10583952dc
Norani375 <manochehr.mb@gmail.com>
2026-08-20 06:30:30 +0430

    refactor(dab): use shared official header in forms workspace

--- components/DabStandardFormsWorkspace.tsx
+++ components/DabStandardFormsWorkspace.tsx
@@ -1,1 +1,13 @@
-export { default } from './DabUnifiedOfficialFormsWorkspace';
+'use client';
+
+import DabOfficialHeader from './DabOfficialHeader';
+import DabUnifiedOfficialFormsWorkspace from './DabUnifiedOfficialFormsWorkspace';
+
+export default function DabStandardFormsWorkspace() {
+  return (
+    <div className="dab-standard-forms-shell" dir="rtl">
+      <DabOfficialHeader />
+      <DabUnifiedOfficialFormsWorkspace />
+    </div>
+  );
+}
