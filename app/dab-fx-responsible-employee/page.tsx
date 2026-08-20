b8613c8284b60ac5757136be56e7f1f6fb0b3c6a
Norani375 <manochehr.mb@gmail.com>
2026-08-20 06:18:47 +0430

    feat(dab): add FX responsible employee route

---
+++ app/dab-fx-responsible-employee/page.tsx
@@ -0,0 +1,5 @@
+import DabFxResponsibleEmployeeForm from '@/components/DabFxResponsibleEmployeeForm';
+
+export default function DabFxResponsibleEmployeePage() {
+  return <DabFxResponsibleEmployeeForm />;
+}
