161fe0b754ef878e3f6bae758afbbf94c3960b13
Norani375 <manochehr.mb@gmail.com>
2026-08-22 03:10:39 -0700

    refactor(dab): activate official renewal form structure

--- app/dab-renewal/page.tsx
+++ app/dab-renewal/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import DabStandardFormsWorkspace from '@/components/DabStandardFormsWorkspace';
+import DabLicenseRenewalOfficialForm from '@/components/DabLicenseRenewalOfficialForm';
 import DabRenewalApplicationWorkflow from '@/components/DabRenewalApplicationWorkflow';
 import DabRenewalFinalReport from '@/components/DabRenewalFinalReport';
 import { useCompany } from '@/lib/companyContext';
@@ -9,7 +9,7 @@ export default function DabRenewalPage() {
   const { activeCompanyId } = useCompany();
   return (
     <main className="space-y-6">
-      <DabStandardFormsWorkspace />
+      <DabLicenseRenewalOfficialForm companyId={activeCompanyId} />
       <DabRenewalApplicationWorkflow companyId={activeCompanyId} />
       <DabRenewalFinalReport companyId={activeCompanyId} />
     </main>
